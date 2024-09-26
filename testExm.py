8264a317987cd3b95a8a25b92169a1ca639bad2b
tester <zeetech1022@gmail.com>
2024-09-26 16:44:16 +0300

    first test

--- testExm.py
+++ testExm.py
@@ -1,8 +1,12 @@
+from PIL.ImageOps import expand
+
+
 class TestExample:
     def test_check_math(self):
         a = 5
         b = 9
-        assert a+b == 14
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables is not qual to {expected_sum}"
 
     def test_check_math2(self):
             a = 5

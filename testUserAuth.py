55e338be6128d03e81617a35fb50fbcc60d4d22f
tester <zeetech1022@gmail.com>
2024-10-10 12:59:12 +0300

    first test

--- testUserAuth.py
+++ testUserAuth.py
@@ -1,35 +1,32 @@
-from http.client import responses
 
+import pytest
 import requests
-from httplib2.auth import auth_param
-from uaclient.files.user_config_file import user_config
 
 
-class TestUserAuth:
-    def test_auth_user(self):
-        data = {
-            'email': 'vinkotov@example.com',
-            'password':'123'
-        }
+class TestFirstApi:
+    names = [
+        ("Slava"),
+        ("Seva"),
+        ("")
+    ]
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data = data )
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
+        url = "https://playground.learnqa.ru/api/hello"
+        data = {'name': name}
 
-        assert  "auth_sid" in response1.cookies , "no cookie"
-        assert  "x-csrf-token" in response1.headers ,  "no headers"
-        assert "user_id" in response1.json() , "no user"
+        response = requests.get(url, params = data )
 
-        auth_sid = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x-csrf-token")
-        user_id_from_auth_method = response1.json()["user_id"]
+        assert response.status_code == 200, "Wrong response code"
 
-        response2  = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token": token} ,
-            cookies={"auth_sid":auth_sid}
-        )
-assert "user_id" in response2.json(), "no user in 2nd response "
-user_id_from_check_method = response2.json()["user_id"]
-print(user_id_from_check_method)
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no filed 'answer' in the response"
 
-assert user_id_from_check_method == user_id_from_check_method
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
 
+        else:
+            expected_response_text  = f"Hello, {name}"
+
+        actual_response_text = response_dict["answer"]
+        assert expected_response_text == actual_response_text , "actual text in the response is not correct"

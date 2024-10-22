d706359109be4944ec8a8f59d9e1a815f012a728
tester <zeetech1022@gmail.com>
2024-10-22 17:25:07 +0300

    first test

--- lib/base_case.py
+++ lib/base_case.py
@@ -8,18 +8,19 @@ from requests import Response
 # noinspection PyUnreachableCode
 class BaseCase:
     def get_cookie(self, resposne: Response, cookie_name):
-    assert cookie_name in response.cookies , f"cannot find cookie with name {cookie_name} in the last response"
-    return response.cookies[cookie_name]
+        assert cookie_name in response.cookies , f"cannot find cookie with name {cookie_name} in the last response"
+        return response.cookies[cookie_name]
 
-    def get_header(self, response: Repsonse, headers_name):
+    def get_header(self, response: Response, headers_name):
         assert headers_name in response.header, f"Cannot find header with the name {headers_name} in the last response"
         return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
         try:
-            response_as_dict = response.json()
+            response_as_dict =response.json()
         except json.decoder.JSONDecoderError:
             assert  False , f"Response is not json. Response test '{response.text}' "
 
-            assert  name in response_as_dict , f"Repsponse Jsonm dosnt have key '{name}'"
-            return response_as_dict[name]
+        assert  name in response_as_dict , f"Repsponse json dosnt have key '{name}'"
+
+        return response_as_dict[name]
--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -17,9 +17,10 @@ class TestUserAuth(BaseCase):
             }
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
-        self.auth_sid = response1.cookies.get("auth_sid")
-        self.token = response1.headers.get("x-csrf-token")
+        self.auth_sid = self.get_cookie(response1,"auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
     def test_auth_user(self):
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
